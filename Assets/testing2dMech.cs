4381d132370cddf9b49a22cdf29812935bac5ebf
ciaran3001 <ciarankelly15@hotmail.com>
2015-12-21 21:28:28 +0000

    fixing weird bug

--- Assets/testing2dMech.cs
+++ Assets/testing2dMech.cs
@@ -24,11 +24,12 @@ public class testing2dMech : MonoBehaviour {
 	
 
 	void Update () {
-<<<<<<< HEAD
 		
 		if(isThreeD){
 		
-		}else{
+		}
+		else
+		{
 			if (Input.GetKeyDown ("space")){
 				if(onGround){
 					sprite.GetComponent<Rigidbody>().AddForce(Vector2.up * 500);
@@ -59,24 +60,22 @@ public class testing2dMech : MonoBehaviour {
 		}
 		if(movingRight){
 			moveRight();
-=======
+		}
 		if(!isThreeD){
-			sprite.transform.position.z = 0;
-		
 			if (Input.GetKeyDown ("space")){
 				if(onGround){
-					sprite.rigidbody.AddForce(Vector2.up * 500);
+					sprite.GetComponent<Rigidbody>().AddForce(Vector2.up * 500);
 					onGround = false;
 				}
 			}
 		
 			if(Input.GetKeyDown ("a")){
 			print("a");
-				sprite.rigidbody.AddForce(Vector2.right * -100);
+				sprite.GetComponent<Rigidbody>().AddForce(Vector2.right * -100);
 			}
 			if(Input.GetKeyDown ("d")){
 			print("d");
-				sprite.rigidbody.AddForce(Vector2.right * 100);
+				sprite.GetComponent<Rigidbody>().AddForce(Vector2.right * 100);
 			}
 		
 			if(Input.GetKeyDown ("c")){
@@ -84,22 +83,16 @@ public class testing2dMech : MonoBehaviour {
 					cameraTwoD.SetActive(true);
 					cameraThreeD.SetActive(false);
 				
-					spriteThreeD.SetActive(false);
-					spriteTwoD.SetActive(true);
-				
 				}else{
 					cameraTwoD.SetActive(false);
 					cameraThreeD.SetActive(true);
 				
-					spriteTwoD.SetActive(false);
-					spriteThreeD.SetActive(true);
 				}
 			}
 		}else{
 		//3d controller
->>>>>>> 31437d1fb31d938b320a8a17164f44e3a03b82a8
+
 		}
-		
 	}
 	void OnCollisionEnter(Collision collision){
 		if(collision.gameObject.tag == "ground"){
