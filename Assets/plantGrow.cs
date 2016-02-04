04c686e7edbe84887a35826fa473e48a1fd74920
unknown <x15322806@SLABS15-16-0002.student.nci.col>
2016-02-04 10:21:35 +0000

    hh

---
+++ Assets/plantGrow.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+public class plantGrow : MonoBehaviour {
+	/*	plan:
+	
+		Hold down left, grow. Differentiate between bouncy or bridge.
+		Hold right, shrink.
+		
+	
+	*/
+	float range;
+	bool growing = false;
+	int plantType;
+	float timeStart;
+	float timeFinish;
+	
+	void Start(){
+		
+	}
+	void Update(){
+		
+		
+	}
+	
+	 void GrowPlanr(){
+		 
+		 
+	 }
+	 
+	 
+}
+	
