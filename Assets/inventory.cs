d57a02ba6ce3ede21b1191498bb5ae01fcdea534
unknown <x15322806@SLABS15-16-0002.student.nci.col>
2016-02-04 09:56:36 +0000

    update

--- Assets/inventory.cs
+++ Assets/inventory.cs
@@ -2,10 +2,25 @@ using UnityEngine;
 using System.Collections;
 
 public class inventory : MonoBehaviour {
+	
+	int animalFrag = 0;
+	int amPlantGrow = 0;
+	
 	void start(){
 	
 	}
+	/*
+	HOLD left mous button to grow plant. [In Range?]
+	The longer you hold it the more it grows.
+	Hold the right mouse button to shrink plant
+	*/
 	void Update(){
-	
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+		
+		if(Input.GetMouseButtonDown(0)){
+			if (Physics.Raycast(transform.position, fwd, 10)) {
+				
+			}
+		}	
 	}
 }
